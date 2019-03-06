bcb79edb222ac949f6633c9606c8f839ea276308
tomoya12 <peng4766@gmail.com>
2019-03-06 23:31:25 +0800

    new pages

---
+++ pages/live-info/index.js
@@ -0,0 +1,25 @@
+// pages/live-info/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    teams: ['Lamigo', '711'],
+    scores: [
+      [1, 3],
+      [1, 3],
+      [1, 3],
+      [1, 3],
+      [1, 3],
+      [1, 3],
+      [1, 3],
+      [0, 0]
+    ],
+    lives: [
+      "gggggggggggg",
+      "hhhhhhhhhhhhh"
+    ]
+  },
+
+})
