f2d4c44a2835c6c788c6e40d731a90323e2dbe79
tomoya12 <peng4766@gmail.com>
2019-05-18 21:46:11 +0800

    final

--- src/views/LiveBroadcasting.vue
+++ src/views/LiveBroadcasting.vue
@@ -2,43 +2,92 @@
   <v-container grid-list-xs>
     <v-layout row wrap>
       <v-flex sm12>
-        <h5 class="headline mb-4">Live Broadcasting</h5>
+        <h5 class="headline mb-4">
+          {{ formState == 'live' ? 'Live Broadcasting': 'Chit Chat'}}
+        </h5>
+        
         <v-form ref="form">
           <v-layout row wrap>
             <v-flex sm12>
-              <v-select
-                :items="rooms"
-                v-model="selectedRoom"
-                item-text="name"
-                item-value="id"
-                label="Live Room"
-              ></v-select>
+              <v-radio-group v-model="formState" row>
+                <v-radio label="Live" value="live"></v-radio>
+                <v-radio label="Chit Chat" value="chat"></v-radio>
+              </v-radio-group>
             </v-flex>
           </v-layout>
-          <v-layout row wrap>
-            <v-flex>
-              <v-text-field
-                v-model="newCasting"
-                label="New Casting"
-              ></v-text-field>
-            </v-flex>
-            <v-flex>
-              <v-btn
-                color="info"
-                @click="submitCasting"
-              >Submit</v-btn>
-            </v-flex>
-          </v-layout>
+
+          <template v-if="formState === 'live'">
+            <div>
+              <v-layout row wrap>
+                <v-flex sm6>
+                  <v-select
+                    :items="rooms"
+                    v-model="selectedRoom"
+                    item-text="name"
+                    item-value="id"
+                    label="Live Room"
+                  ></v-select>
+                </v-flex>
+              </v-layout>
+
+              <v-layout row wrap>
+                <v-flex>
+                  <v-text-field
+                    v-model="newCasting"
+                    label="New Casting"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-btn
+                    color="info"
+                    @click="submitCasting"
+                  >Submit</v-btn>
+                </v-flex>
+              </v-layout>
+            </div>
+          </template>
+
+          <template v-if="formState === 'chat'">
+            <div>
+              <v-layout row wrap>
+                <v-flex sm6>
+                  <v-text-field
+                    v-model="fakeUsername"
+                    label="Pretend to be..."
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row wrap>
+                <v-flex>
+                  <v-text-field
+                    v-model="newChat"
+                    label="Just Say Something"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-btn
+                    color="info"
+                    @click="submitChat"
+                  >SEND</v-btn>
+                </v-flex>
+              </v-layout>
+            </div>
+          </template>
         </v-form>
       </v-flex>
       
       <v-flex sm12>
         <v-card>
           <v-list>
-            <v-list-tile v-for="(cast, index) in castingList" :key="index">
+            <v-list-tile v-for="(cast, index) in filterCastingList" :key="index">
               <v-layout row wrap>
-                <v-flex sm4>
-                    <v-list-tile-content>{{cast.time}} : </v-list-tile-content>
+                <v-flex sm3>
+                  <v-list-tile-content>{{cast.time}} : </v-list-tile-content>
+                </v-flex>
+                <v-flex sm1>
+                  <v-list-tile-content>
+                    {{cast.type === 'live' ? '📢📢📢' : '😀😀😀'}}
+                  </v-list-tile-content>
                 </v-flex>
                 <v-flex sm8>
                   <v-list-tile-content>{{cast.msg}}</v-list-tile-content>
@@ -62,39 +111,49 @@ export default {
         name: 'CENTER LIVE ROOM',
         id: 1
       }],
+      formState: 'live',
       selectedRoom: 1,
       newCasting: '',
+      fakeUsername: 'admin00',
+      newChat: '',
       castingList: [],
       clientio: null,
     }
   },
   methods: {
     submitCasting() {
-      this.sendCasting();
-    },
-    resetForm() {
-      this.newCasting = '';
-    },
-
-    sendCasting() {
       this.clientio.send(JSON.stringify({
         by: this.rooms[0].name,
         type: 'live',
         msg: this.newCasting,
       }))
       this.resetForm();
+    },
+    resetForm() {
+      this.newCasting = '';
+      this.newChat = '';
+    },
+
+    submitChat() {
+      this.clientio.send(JSON.stringify({
+        by: this.fakeUsername || 'admin00',
+        type: 'chat',
+        msg: this.newChat,
+      }))
+      this.resetForm();
+    },
+  },
+
+  computed: {
+    filterCastingList() {
+      if (this.formState === 'live') {
+        return this.castingList.filter(item => item.type !== 'chat');
+      }
+      return this.castingList;
     }
   },
-  created() {
-    // const clientio = io('http://localhost:3000/gamelive');
-    // clientio.on('connect', () => {
-    //   this.$toast(`CONENCTED TO WEBSOCKET`);
-    // })
-    // clientio.on('message', (data) => {
-      //   console.log(data);
-    // })
-    // this.clientio = clientio;
 
+  created() {
     const ws = new WebSocket(this.wshost);
     ws.onopen = () => {
       this.$toast(`CONENCTED TO WEBSOCKET`);
@@ -110,6 +169,7 @@ export default {
 
   destroyed() {
     this.clientio.close();
+    this.$toast(`DISCONENCTED FROM WEBSOCKET`);
   }
 }
 </script>
--- src/views/UserComment.vue
+++ src/views/UserComment.vue
@@ -10,16 +10,16 @@
               :key="index"
             >
               <v-list-tile-avatar>
-                <img :src="cmt.user.avatar">
+                <img :src="''">
               </v-list-tile-avatar>
 
               <v-list-tile-content>
-                <v-list-tile-title>{{cmt.msg}}</v-list-tile-title>
-                <v-list-tile-sub-title>{{cmt.user.title}} @{{cmt.date}} {{cmt.time}}</v-list-tile-sub-title>
+                <v-list-tile-title>{{cmt.c_content}}</v-list-tile-title>
+                <v-list-tile-sub-title>{{cmt.c_u_name}} @{{cmt.c_n_title}}</v-list-tile-sub-title>
               </v-list-tile-content>
 
               <v-list-tile-action>
-                <v-btn icon @click="deleteCmt(cmt.id)">
+                <v-btn icon @click="deleteComment(cmt.c_id)">
                   <v-icon>delete</v-icon>
                 </v-btn>
               </v-list-tile-action>
@@ -31,35 +31,29 @@
   </v-container>
 </template>
 <script>
-const fakeCommentList = [{
-  user: {
-    title: 'tomoya01',
-    avatar: '',
-  },
-  msg: 'This is a message',
-  date: '2019-01-01',
-  time: '19:00',
-  id: 1
-}, {
-  user: {
-    title: 'tomoya01',
-    avatar: '',
-  },
-  msg: 'This is another message',
-  date: '2019-01-01',
-  time: '19:00',
-  id: 2
-}];
 export default {
   data() {
     return {
-      commentList: fakeCommentList,
+      commentList: [],
     }
   },
+  created() {
+    this.fetchComment();
+  },
   methods: {
-    deleteCmt(idx) {
-      this.commentList = this.$_.filter(this.commentList, item => item.id !== idx);
-      this.$toast(`Comment ${idx} has been removed. `);
+    fetchComment() {
+      this.axios.get(this.host+'/news/allcomment').then(({ data }) => {
+        this.commentList = data.results;
+      })
+    },
+    deleteComment(commentid) {
+      this.axios.post(this.host+'/news/deletecomment', this.qs.stringify({
+        commentid
+      })).then(() => {
+        this.$toast(`Comment has been removed. `);
+      }).finally(() => {
+        this.fetchComment();
+      })
     }
   }
 }
